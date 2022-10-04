d4d751d8aa8934bd69a109ca82bf2a3ae7e61b0f
Mgamboa <marianellag400@gmail.com>
2022-10-04 16:34:20 -0500

    added packages that where required, throwing error in terminal, also started in mysql layout and inserting seed info

---
+++ Develope/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles (id, roles_title, roles_salary)
+VALUES ("Sales", "80000"),
+       ("Engineering", "150000"),
+       ("Finance", "125000");
---
+++ Develope/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        password: 'root',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the classlist_db database.`)
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
